066c637d6482e7f7744eecaca69a944007874875
Ahmedayaz1210 <ayazm@wit.edu>
2025-01-02 18:25:23 -0500

    Added comments

--- doc_processor.py
+++ doc_processor.py
@@ -3,11 +3,19 @@ import json
 
 
 def clean_docx(doc, residents):
+    """
+    This function takes a docx file and a list of residents as input and extracts the residents' information from the docx file.
+    The function then appends the residents' information to the list of residents.
+    """
+
     for para in doc.paragraphs:
-        
         if para.text.strip() and not para.text.__contains__('Residents on Selective Menu:'):
+            
+            # Split the text into two parts: Patient Name and Room Number/Diet Type
             parts = para.text.split('Rm.')
-            if len(parts) == 2:  # Make sure we found "Rm."
+            # If the text contains 'Rm.' then it has two parts
+            if len(parts) == 2:  
+                # Split the second part into Room Number and Diet Type
                 room_diet = parts[1].strip() 
                 room_diet_parts = room_diet.split()
                 resident = {
@@ -16,10 +24,10 @@ def clean_docx(doc, residents):
                     "Diet Type": room_diet_parts[1].strip()
                 }
                 residents.append(resident)
+
     return residents
                 
-                     
-    
+                    
 
 doc = docx.Document('input_files/menus.docx')
 residents = []
